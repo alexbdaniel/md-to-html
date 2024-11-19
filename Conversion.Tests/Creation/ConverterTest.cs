20ea2f75f22c47559a71aa364d70300b2ffc084a
Daniel, Alex <alex.daniel@health.wa.gov.au>
2024-11-19 13:16:46 +0800

    init

---
+++ Conversion.Tests/Creation/ConverterTest.cs
@@ -0,0 +1,16 @@
+using Conversion.Creation;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Conversion.Tests.Creation;
+
+[TestSubject(typeof(Converter))]
+public class ConverterTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
